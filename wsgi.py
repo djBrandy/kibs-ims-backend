4d1c77c1ea80e99ce71d13328629785f37712cbc
djBrandy <dandobrandon0@gmail.com>
2025-06-24 11:21:46 +0300

    fixes made

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,4 @@
 from app import app
-from app.database import db, migrate
-from flask.cli import FlaskGroup # type: ignore
-
-def create_app():
-    return app
-
-cli = FlaskGroup(create_app=create_app)
 
 if __name__ == '__main__':
-    cli()
+    app.run()
